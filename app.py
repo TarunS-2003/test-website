6f1fbeb7665febd71583232199054b95857b6572
TarunS-2003 <95118533+TarunS-2003@users.noreply.github.com>
2023-05-13 14:09:18 +0000

    Added login and signup.

--- app.py
+++ app.py
@@ -1,21 +1,14 @@
-from flask import Flask,render_template
+from flask import Flask
+from flask import render_template
 app=Flask(__name__)
 @app.route("/")
-def hello_world():
-  details=[
-    {"id":1,
-      "name":"seneca",
-      "address":"15,SF,USA"
-    },
-    {"id":1,
-      "name":"seneca",
-      "address":"15,SF,USA"
-    },
-    {"id":1,
-      "name":"seneca",
-      "address":"15,SF,USA"
-    }
-  ]
-  return render_template('index.html',det=details)
+def landing():
+  return render_template('index.html')
+@app.route("/login")
+def login():
+  return render_template('login.html')
+@app.route("/signup")
+def signup():
+  return render_template('signup.html')
 if __name__=="__main__":
   app.run(host="0.0.0.0",debug=True)
