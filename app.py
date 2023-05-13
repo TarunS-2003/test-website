fd06789d34f6334791e20945ba02edeb85acbbd8
TarunS-2003 <95118533+TarunS-2003@users.noreply.github.com>
2023-05-13 14:00:41 +0000

    Added Login and Signup.

--- app.py
+++ app.py
@@ -2,6 +2,20 @@ from flask import Flask,render_template
 app=Flask(__name__)
 @app.route("/")
 def hello_world():
-  return render_template('index.html')
+  details=[
+    {"id":1,
+      "name":"seneca",
+      "address":"15,SF,USA"
+    },
+    {"id":1,
+      "name":"seneca",
+      "address":"15,SF,USA"
+    },
+    {"id":1,
+      "name":"seneca",
+      "address":"15,SF,USA"
+    }
+  ]
+  return render_template('index.html',det=details)
 if __name__=="__main__":
   app.run(host="0.0.0.0",debug=True)
